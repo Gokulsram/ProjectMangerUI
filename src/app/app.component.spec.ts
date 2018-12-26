8de4204ed47882929e7115fcb5365aaace7535ab
Gokulsram <gokulsram@gmail.com>
2018-12-26 10:42:54 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TaskManagementSystem'`, () => {
+  it(`should have as title 'Project Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TaskManagementSystem');
+    expect(app.title).toEqual('Project Management');
   });
 
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to TaskManagementSystem!');
-  // });
 });
