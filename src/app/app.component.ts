bb23c782af241dee169583467f2a2c482a07ab4c
Gokulsram <gokulsram@gmail.com>
2018-12-26 10:29:24 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TaskManagementSystem';
+
+  constructor(private router: Router) {
+    this.DefaultStyle();
+    this.adduserstyle = "view-task btn cust-add-user cust-task mr-3 cust-add-user-active";
+  }
+
+  adduserstyle: string;
+  addtaskstyle: string;
+  addprjectstyle: string;
+  viewtaskstyle: string;
+
+  AddUser() {
+    this.DefaultStyle();
+    this.adduserstyle = "view-task btn cust-add-user cust-task mr-3 cust-add-user-active";
+    this.router.navigate(['/']);
+  }
+
+  AddProject() {
+    this.DefaultStyle();
+    this.addprjectstyle = "view-task btn cust-add-project cust-task mr-3 cust-add-project-active";
+    this.router.navigate(['/addproject']);
+  }
+
+  AddTask() {
+    this.DefaultStyle();
+    this.addtaskstyle = "add-task btn mr-2 cust-add-task cust-task mr-3 cust-add-task-active";
+    this.router.navigate(['/addtask']);
+  }
+
+  ViewTask() {
+    this.DefaultStyle();
+    this.viewtaskstyle = "view-task btn cust-view-task cust-task cust-view-task-active";
+    this.router.navigate(['/viewtask']);
+  }
+
+  DefaultStyle() {
+    this.adduserstyle = "view-task btn cust-add-user cust-task mr-3";
+    this.addtaskstyle = "add-task btn mr-2 cust-add-task cust-task mr-3";
+    this.addprjectstyle = "view-task btn cust-add-project cust-task mr-3";
+    this.viewtaskstyle = "view-task btn cust-view-task cust-task";
+  }
+
 }
